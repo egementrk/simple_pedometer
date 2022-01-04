df53c18b1027847cf4a75e3b8b2dcf0e7a63c763
egementrk <turk.egemen@hotmail.com>
2022-01-04 18:05:59 +0300

    core data prepare

--- simple_pedometer/VC/MainVC.swift
+++ simple_pedometer/VC/MainVC.swift
@@ -9,8 +9,16 @@ import UIKit
 import CoreMotion
 //Core Motion reports motion- and environment-related data
 //from the onboard hardware of iOS devices
+import CoreData
+
+let appDelegate = UIApplication.shared.delegate as! AppDelegate
+let context = appDelegate.persistentContainer.viewContext
+
 
 class MainVC: UIViewController {
+    
+    let stepData = Step(context: context)
+    var step = [Step]()
     
     @IBOutlet weak var activityValue: UILabel!
     @IBOutlet weak var stepValue: UILabel!
@@ -20,14 +28,14 @@ class MainVC: UIViewController {
 
     private let pedometer = CMPedometer()
     //For fetching the system-generated live walking data.
-    let testWalking = CMMotionActivity()
+    //let testWalking = CMMotionActivity()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //stepData.current_step = "15"
         
+        // Do any additional setup after loading the view.
         if CMMotionActivityManager.isActivityAvailable(){
             //if detect any activity
             self.activityManager.startActivityUpdates(to: OperationQueue.main){
@@ -45,23 +53,39 @@ class MainVC: UIViewController {
             }
         }
         
+        
         if CMPedometer.isStepCountingAvailable(){
             self.pedometer.startUpdates(from: Date()){(data ,error) in
                 if error == nil {
                     if let response = data {
                         DispatchQueue.main.async {
                             self.stepValue.text = "\(response.numberOfSteps)"
+                            self.stepData.current_step = self.stepValue.text
+                            //appDelegate.saveContext()
                         }
                     }
                 }
             }
         }
     }
+    override func viewWillAppear(_ animated: Bool){
+            if stepData.current_step == nil {
+                stepValue.text = "None"
+            }else{
+                stepValue.text = stepData.current_step!
+            }
+    }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        stepData.current_step = "15"
+        do {
+            print("Başarılı")
+            try context.save()
+        } catch  {
+            print("error")
+        }
+    }
 
-    /*
-    // MARK: - Navigation
 
-    */
 
 }
